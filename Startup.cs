5333dec7e4ea4be36b026d4a938ab6bb0402c283
annettet <atumukunde@wys.rw>
2024-05-14 14:43:06 +0200

    Pushing wedding invitees application

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingInviteesApp.Startup))]
+namespace WeddingInviteesApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
